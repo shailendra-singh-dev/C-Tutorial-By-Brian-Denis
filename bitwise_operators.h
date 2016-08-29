be834f1685a1ebcdac45550b4e942b54ace922a8
Shailendra Singh <ssingh@itexico.net>
2016-08-29 11:42:05 -0500

    Added files for poiner

---
+++ bitwise_operators.h
@@ -0,0 +1,11 @@
+#ifndef BITWISE_OPERATORS
+#define BITWISE_OPERATORS
+
+int bitwise_and();
+int bitwise_or();
+int bitwise_xor();
+int bitwise_not();
+int bitwise_left_shift();
+int bitwise_right_shift();
+
+#endif
---
+++ bitwise_operators_main.c
@@ -0,0 +1,6 @@
+#include "bitwise_operators.h"
+#include "stdio.h"
+
+void main(){
+	bitwise_left_shift();
+}

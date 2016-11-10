8b8178c2e565d46954ee5b62846d3022c4344cb8
Shailendra Singh <shail.friendly@gmail.com>
2016-11-10 15:10:18 -0600

    Update macros.c

--- macros.c
+++ macros.c
@@ -1,10 +1,25 @@
 #include "stdio.h"
 
-void main() {
-	const volatile int i = 100;
-	int* ptr;
-	ptr = &i;
-	printf("%d\n", i);
-	*ptr = 200;
-	printf("%d", i);
+#include "stdio.h"
+#define MULTIPLY(X,Y) X*Y
+#define INCREMENT(x)  ++x
+#define merge(a,b) a##b
+#define get(a) #a
+#define print(i,limit) while(i<=limit){\
+						printf("%d",i);\
+						i++;\
+					   }
+
+int main() {
+	printf("%d\n", MULTIPLY(10,10));
+	int x = 100;
+	char *str = "abc";
+	printf("%d\n", INCREMENT(x));
+	printf("%s\n", INCREMENT(str));
+	printf("%d\n", merge(123,567));
+	printf("%s\n", get(123));
+
+	int i = 1;
+	print(i,10);
+	return 0;
 }
